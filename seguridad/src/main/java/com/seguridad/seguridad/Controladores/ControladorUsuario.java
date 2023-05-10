4be1155c32d13ece735ac7766412fee52030c886
Jennyfer Lopez <jennyferlopezllanten@gmail.com>
2023-05-10 01:05:12 -0500

    Subiendo controlador usuario crear - eliminar

--- seguridad/src/main/java/com/seguridad/seguridad/Controladores/ControladorUsuario.java
+++ seguridad/src/main/java/com/seguridad/seguridad/Controladores/ControladorUsuario.java
@@ -1,6 +1,52 @@
 package com.seguridad.seguridad.Controladores;
+import com.seguridad.seguridad.Modelos.Usuario;
+import com.seguridad.seguridad.Repositorios.RepositorioUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
+@CrossOrigin
+@RestController
+@RequestMapping("/usuarios")
 public class ControladorUsuario {
+    @Autowired
+    private RepositorioUsuario miRepositorioUsuario;
 
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Usuario create(@RequestBody Usuario infoUsuario){
+        infoUsuario.setContrasena(convertirSHA256(infoUsuario.getContrasena()));
+        return this.miRepositorioUsuario.save(infoUsuario);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Usuario usuarioActual=this.miRepositorioUsuario
+                .findById(id)
+                .orElse(null);
+        if (usuarioActual!=null){
+            this.miRepositorioUsuario.delete(usuarioActual);
+        }
+    }
+    public String convertirSHA256(String password) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+        byte[] hash = md.digest(password.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for(byte b : hash) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
 }
